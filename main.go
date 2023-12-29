18d50c486c43186d9db6de6919a06c76c33a7067
Antony Nyagah <tony.m.nyagah@gmail.com>
2023-12-29 19:03:49 +0300

    start adding the templating system

--- main.go
+++ main.go
@@ -4,18 +4,48 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"strings"
+	"text/template"
 )
 
 type Domain struct {
-	Name   string
-	Status string
+	Name              string
+	StatusCode        int
+	CertificateExpiry string
 }
 
 func main() {
+	port := ":8080"
+	log.Println("Starting server on port", strings.Split(port, ":")[1])
+
 	healthCheck := func(w http.ResponseWriter, r *http.Request) {
 		io.WriteString(w, "It works!")
 	}
 	http.HandleFunc("/healthcheck", healthCheck)
 
-	log.Fatal(http.ListenAndServe(":8090", nil))
+	handler1 := func(w http.ResponseWriter, r *http.Request) {
+		tmpl := template.Must(template.ParseFiles("index.html"))
+		domains := map[string][]Domain{
+			"Domains": {
+				{
+					Name:              "huqas.org",
+					StatusCode:        200,
+					CertificateExpiry: "June 20, 2024",
+				},
+			},
+		}
+		tmpl.Execute(w, domains)
+	}
+
+	handler2 := func(w http.ResponseWriter, r *http.Request) {
+		name := r.PostFormValue("name")
+		tmpl := template.Must(template.ParseFiles("index.html"))
+
+		tmpl.ExecuteTemplate(w, "domain-list-element", Domain{Name: name, StatusCode: 200, CertificateExpiry: "June 20, 2024"})
+	}
+
+	http.HandleFunc("/", handler1)
+	http.HandleFunc("/add-domain", handler2)
+
+	log.Fatal(http.ListenAndServe(port, nil))
 }
